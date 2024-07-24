d855e5e9156d5db7f0ba23a862e893eea26ef373
Anuragtk117 <anuragtk117@gmail.com>
2024-07-24 16:59:03 +0530

    reduce the migration files usergroups - usercommunity

--- migrations/20240723101743-create-groups.js
+++ migrations/20240723101743-create-groups.js
@@ -35,8 +35,51 @@ module.exports = {
         type: Sequelize.DATE,
       },
     });
+    await queryInterface.createTable("UserGroups", {
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "UserS",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        primaryKey: true,
+      },
+      groupId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Groups",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        primaryKey: true,
+      },
+      userType: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.addConstraint("UserGroups", {
+      fields: ["userId", "groupId"],
+      type: "unique",
+      name: "unique_user_group",
+    });
   },
   async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("UserGroups", "unique_user_group");
+    await queryInterface.dropTable("UserGroups");
     await queryInterface.dropTable("Groups");
   },
 };
--- migrations/20240724053010-create-communities.js
+++ migrations/20240724053010-create-communities.js
@@ -1,39 +1,84 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Communities', {
+    await queryInterface.createTable("Communities", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       communityName: {
         type: Sequelize.STRING,
-        allowNull:false,
-        unique:true
+        allowNull: false,
+        unique: true,
       },
       profilePhoto: {
         type: Sequelize.STRING,
-        allowNull:true
-        
+        allowNull: true,
       },
       creatorId: {
         type: Sequelize.INTEGER,
-        allowNull:false
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable("userCommunities", {
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        primaryKey: true,
+      },
+      communityId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Communities",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        primaryKey: true,
+      },
+      userType: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.addConstraint("userCommunities", {
+      fields: ["userId", "communityId"],
+      type: "unique",
+      name: "unique_user_community",
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Communities');
-  }
+    await queryInterface.removeConstraint(
+      "userCommunities",
+      "unique_user_community"
+    );
+    await queryInterface.dropTable("userCommunities");
+    await queryInterface.dropTable("Communities");
+  },
 };
